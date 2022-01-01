3baf026b98e968d25b0f229e4baf9f95c54c6ffc
khaled-saeed <kahled.saeed.tawfik@gmail.com>
2022-01-01 10:46:10 +0200

    new commit

---
+++ ADC/ADC_BSP.h
@@ -0,0 +1,15 @@
+#ifndef __ADC_BSP_H__
+#define __ADC_BSP_H__
+
+
+#include <stdint.h>
+
+#define PORT_E (1U<<4) 
+#define AN0 (1U<<3) 
+#define SS3 (1U<<3) 
+void ADC_INIT (void); 
+
+
+
+
+#endif //__ADC_BSP_H__
