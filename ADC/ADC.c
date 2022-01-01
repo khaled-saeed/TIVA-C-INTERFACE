c14b099fe4f29830987b9b1689e3d78066a492dc
khaled-saeed <kahled.saeed.tawfik@gmail.com>
2022-01-01 14:23:54 +0200

    ADC MODULE

--- ADC/ADC.c
+++ ADC/ADC.c
@@ -1,26 +1,37 @@
 #include "TM4C123.h"                    // Device header
-#include "ADC_BSP.h" 
+#include "ADC_BSP.h"
+/**
+!brief : initializing ADC0 using one sample with SS3 @ sampling rate 250 KSPS the sampling starts using interrupt
+*/
 
-
-void ADC_INIT (void)
+void ADC0_INIT (void)
 {
-	SYSCTL->RCGCADC |= 0x01 ; // ADC0 
-	SYSCTL->RCGCGPIO |= PORT_E ;  // AN0 --> PE3 
+	SYSCTL->RCGCADC |= ADC0 ; 				// Enable ADC0 clock
+	SYSCTL->RCGCGPIO |= PORT_E ;  			// Enable PORTE clock
 
 	/*************  The GPIO pin for  ADC *****************/
-	GPIOE->AFSEL |= AN0 ; 
-	GPIOE->DEN &= ~AN0 ; 
-	GPIOE->AMSEL |= AN0 ; 
-	/********** ADC0 sample sequencer config ***************/
-	ADC0->ACTSS &= ~SS3 ;  // start by disabling SS3 before config 
-	ADC0->EMUX &= ~(0xFU<<12); // clear the EM3 to choose trigger sampling via software  by setting SSn bit in ADCPSSI
-	ADC0->SSMUX3 = 0;					// choosing SS3 AND AN0 in SS3 
-	ADC0->SSCTL3 |= (1U<<1)|(1U<<2);// one sample at a time and use interrupt  
-	ADC0->PC = 0x3;						// configure the sampling rate to 250 KSPS 
-	/****** if with interrupt *******/ 
-	ADC0->IM |= SS3 ; 			// enable interrupt 
-	ADC0->ACTSS |= SS3 ; 		// enable SS3 
-	NVIC->ISER[0] |= 0x00020000 ;
+	GPIOE->AFSEL |= AN0 ; 					//Enable alternative function for pin PE3
+	GPIOE->DEN &= ~AN0 ; 					//disable the digital functionality  for pin PE3
+	GPIOE->AMSEL |= AN0 ; 					//Enable analog functionality for PE3
+	/********** ADC0 sample sequencer configuration ***************/
+	ADC0->ACTSS &= ~SS3 ;  					// start by disabling SS3 before configuration
+	ADC0->EMUX &= ~(0xFU<<12); 				// clear the EM3 to choose trigger sampling via software by setting SSn bit in ADCPSSI
+	ADC0->SSMUX3 = 0;						// choosing SS3 AND AN0 in SS3
+	ADC0->SSCTL3 |= (1U<<1)|(1U<<2);		// one sample at a time and use interrupt
+	ADC0->PC = 0x3;							// configure the sampling rate to 250 KSPS
+	/****** if with interrupt *******/
+	ADC0->IM |= SS3 ; 						// enable interrupt
+	ADC0->ACTSS |= SS3 ; 					// enable SS3
+	NVIC->ISER[0] |= 0x00020000 ;			// enable the interrupt in NVIC
 
 }
 
+/** if we're using interrupts we mus include this ISR in the main module
+void ADC0SS3_Handler(void){
+
+	adc_value = ADC0->SSFIFO3; // read adc coversion result from SS3 FIFO
+	ADC0->ISC = 8;          	//clear coversion clear flag bit
+  ADC0->PSSI |= (1<<3);        // Enable SS3 conversion or start sampling data from AN0
+
+}
+*/
--- ADC/ADC_BSP.h
+++ ADC/ADC_BSP.h
@@ -1,13 +1,35 @@
 #ifndef __ADC_BSP_H__
 #define __ADC_BSP_H__
+/** ARM CORTEX M4 has 12 ADC channels
+	AN0 --> PE3
+	AN1 --> PE2
+	AN2 --> PE1
+	AN3 --> PE0
+	AN4 --> PD3
+	AN5 --> PD2
+	AN6 --> PD1
+	AN7 --> PD0
+	AN8 --> PE5
+	AN9 --> PE4
+	AN10 --> PB4
+	AN11 --> PB5
 
+& 	4 Sample Sequencers that is used for sampling control and data capture and allows the ADC
+	To collect the data from multiple sources
+	SEQUENCER 				NO. of samples 				Depth of FIFO
+	SS3							1							1
+	SS2							4							4
+	SS1							4							4
+	SS0							8							8
 
-#include <stdint.h>
+*/
 
-#define PORT_E (1U<<4) 
-#define AN0 (1U<<3) 
-#define SS3 (1U<<3) 
-void ADC_INIT (void); 
+				/**** ADC0 *****/
+#define PORT_E  (1U<<4)
+#define AN0 	(1U<<3)
+#define SS3 	(1U<<3)
+#define ADC0	(1U<<0)
+void ADC0_INIT (void);
 
 
 
